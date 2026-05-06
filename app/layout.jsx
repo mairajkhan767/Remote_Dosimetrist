56d7dea23f4cc8cd2ca9b42fc9a0ec429f2dd6b1
chosenpath-dev <mairajkhan767@outlook.com>
2026-05-06 21:23:23 +0500

    added google tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,25 @@ config.autoAddCss = false
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YYKBF43KHC"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YYKBF43KHC');
+        </script>
+        <meta name="google-site-verification" content="7_n7SeloGYX-tn3PCX6tDGelNFJxobTu6ChgyLQJC88" />
+        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5C8BSRH');</script>
+      </head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5C8BSRH"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <PreloaderProvider>
           <Preloader />
           <Header />
