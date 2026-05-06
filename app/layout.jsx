d43e20d0d70eb937a7721e44e8ce7ae2ad08086c
chosenpath-dev <mairajkhan767@outlook.com>
2026-05-06 21:25:45 +0500

    fixed GA scripts

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import Preloader from '@/components/Preloader'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Popup from '@/components/Popup'
+import Script from "next/script";
 
 config.autoAddCss = false
 
@@ -14,19 +15,34 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YYKBF43KHC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YYKBF43KHC"
+          strategy="afterInteractive"
+        />
 
-          gtag('config', 'G-YYKBF43KHC');
-        </script>
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YYKBF43KHC');
+  `}
+        </Script>
         <meta name="google-site-verification" content="7_n7SeloGYX-tn3PCX6tDGelNFJxobTu6ChgyLQJC88" />
-        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M5C8BSRH');</script>
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),
+      dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M5C8BSRH');
+  `}
+        </Script>
       </head>
       <body>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5C8BSRH"
